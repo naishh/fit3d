3b6f4a9025404a554464b2bc6d2bfe4c8aeb395d
Naish <webnaish@gmail.com>
2010-11-17 12:58:48 +0100

    a

--- m.m
+++ m.m
@@ -1,9 +1,11 @@
 close all;
-% run fit3d setup
-load outputVars_scriptComputeCameraMotion.mat
-load outputVars_Skyline.mat
+% cd ../FIT3D_online_17082010
+% FIT3D_setup
+% cd ../git
+% load outputVars_scriptComputeCameraMotion.mat
+%	%[SkylineX, SkylineY] = getSkyLineMain();
+% load outputVars_Skyline.mat
 
-%[SkylineX, SkylineY] = getSkyLineMain();
 
 % todo make camera center depended of translation in P
 P1 = PcamX(:,:,1) % = [I,0]
@@ -20,26 +22,23 @@ fclose(fp)
 % loop through skyline pixels
 maxI = 400;
 for i=10:10:maxI
-	% the pixel coordinate 
+	% the 2d pixel coordinates of the skyline
 	xy = [SkylineX(i);SkylineY(i)];
 
-	maxV = 100;
 	CC = [0;0;0]
-	lineCoord = pointsTo3DLine(xy, CC, Kcanon10GOOD, maxV)
-	X = lineCoord(:,1)
-	Y = lineCoord(:,2)
-	Z = lineCoord(:,3)
+	lineCoord = pointsTo3DLine(xy, CC, Kcanon10GOOD)
+	X = lineCoord(:,1); Y = lineCoord(:,2); Z = lineCoord(:,3)
+	% plot in matlab
 	plot3(X,Y,Z)
+	xlabel('X axis'); ylabel('Y axis'); zlabel('Z axis')
 	XYZtoObj(X,Y,Z, i)
 	%pause;
 end
 
-% write points
+% write lines in obj file 
 fp = fopen('t.obj', 'a');
 for i=1:maxI
 	fprintf(fp, 'p %d\n', i);
 end
 fclose(fp);
 
-% todo:
-% draw multiple lines in 5d with plot3
--- pointsTo3DLine.m
+++ pointsTo3DLine.m
@@ -1,9 +1,8 @@
-function lineCoord = pointsTo3DLine(xy, CC, K, maxV)
+function lineCoord = pointsTo3DLine(xy, CC, K)
 %% input vars:
 
 % xy, the 2d pixel coordinate
 % CC, the camera center (for first pic this is [0;0;0]
-% maxV, number of 3d points on the line that will be generated
 
 % the homogeneous pixel coordinate 
 xyH = [xy;1];
@@ -14,7 +13,7 @@ xy3D = K * xyH;
 % the direction vector
 xyzDirection = xy3D - CC;
 
-% v presents the position on the line (v = 0 => CC, v = maxV => xy3D)
+% v presents the position on the line (v = 0 => CC, v = 1 => xy3D)
 % 3dLineEq = CC + v * xyzDirection
 
 lineCoord = zeros(2, 3);
