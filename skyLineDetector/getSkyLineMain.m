5ad290b0cf2e5595f532c640640fce32b4c6b9f8
Naish <webnaish@gmail.com>
2010-12-07 10:51:31 +0100

    a

--- skyLineDetector/getSkyLineMain.m
+++ skyLineDetector/getSkyLineMain.m
@@ -1,24 +1,24 @@
-function [SkylineX, SkylineY] = getSkyLineMain()
-close all;
-%for imgNr = 2:5
+function Skylines = getSkyLineMain()
+
+Skylines = cell(5,1);
+for imgNr = 1:5
+%for imgNr = 1:2
 
 	% READ IMAGE 
 	%file = sprintf('dataset/randombuildings/02_gimped.jpg')
-	%file = sprintf('dataset/FloriandeSet1/img/outd%d.jpg', imgNr)
-	file = sprintf('dataset/FloriandeSet1/img/outd0_highcontrast.jpg')
+	file = sprintf('../dataset/FloriandeSet1/img/outd%d.jpg', imgNr)
+	%file = sprintf('../dataset/FloriandeSet1/img/outd0_highcontrast.jpg')
 
 	imRGB = imread(file);
 	imBW = imadjust(rgb2gray(imRGB));
-	figure; imshow(imBW);
-	pause;
+	%figure; imshow(imBW);
 	
 
 	% GAUSSIAN BLUR
-	% todo checken of het wel nut heef te blurren
+	% todo checken of het wel nut heeft te blurren
 	s = fspecial('gaussian',5,5);
 	imBW=imfilter(imBW,s);
-	figure; imshow(imBW);
-	pause;
+	%figure; imshow(imBW);
 
 	%threshtest
 	% for thresh=0.05:0.05:1
@@ -31,7 +31,7 @@ close all;
 	% EDGE DETECTION
 	thresh = 0.05;
 	imEdge = im2double(edge(imBW, 'sobel', thresh));
-	figure; imshow(imEdge)
+	%figure; imshow(imEdge)
 
 	% todo close opening proberen
 
@@ -39,4 +39,7 @@ close all;
 	xStepSize = 1;
 	skylineThresh = 0.9;
 	[SkylineX, SkylineY] = getSkyLine(imRGB, imEdge, xStepSize, skylineThresh);
-%end
+
+	Skylines{imgNr} = struct('SkylineX',SkylineX,'SkylineY', SkylineY) 
+
+end
