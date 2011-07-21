a7e74f3e56a126333770fc7b3e32f7680fd688b7
naishh <webnaish@gmail.com>
2011-07-21 15:13:49 +0200

    a

---
+++ results/lineWallAssociator/lwa.m
@@ -0,0 +1,10 @@
+load Walls
+
+plotBuilding(Walls,[]);
+
+
+% plot (no partial an full overlapping) lines with the buidling
+
+	% search for the front coords
+	% by plotting the lines in plotBuilding one by one
+	% copy coords and change them 
