c8335b049316617de6dee3bbde778ac17e196021
tkosteli <tkosteli@tkostelaptop.(none)>
2011-11-24 16:56:17 +0100

    a

---
+++ scriptProcessModel.m
@@ -0,0 +1,18 @@
+close all;
+setup;
+cd generate3dModel;
+align3dModel;
+pause;
+clear;
+cd ..
+setup;
+cd houghlines3d;
+houghlines3d;
+pause;
+cd ../improveBuilding;
+improveBuilding;
+cd ../project2d;
+project2d;
+cd ..;
+
+
