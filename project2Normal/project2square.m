4dc2c0d69a68b9a5f65f10cbb1599db99ad7bcbb
tkosteli <tkosteli@tkostelaptop.(none)>
2011-11-25 19:33:28 +0100

    a

--- project2Normal/project2square.m
+++ project2Normal/project2square.m
@@ -49,26 +49,56 @@ clear imgOut;
 
 
 %todo calc from 3d transform
-yMax=0.5878;
-yMin=0.1837;
-xMax=1.5380;
-xMin=0.9470;
-% 0.9470   -0.1837
-% 1.5380   -0.1837
-% 0.9470    0.5878
-% 1.5380    0.5878
-yFactor = h/(yMax-yMin)
-xFactor = w/(xMax-xMin)
 
-yFactor = yFactor/100
-xFactor = xFactor/100
 
 PlanePoint0 = [Walls(wallNr,1) Walls(wallNr,2) Walls(wallNr,3)];
 PlanePoint1 = [Walls(wallNr,4) Walls(wallNr,5) Walls(wallNr,6)];
 PlanePoint2 = [Walls(wallNr,7) Walls(wallNr,8) Walls(wallNr,9)];
 
-% todo to repmat structure
-endRange=800;
+% a=1;
+% b=1;
+% % todo to repmat structure
+% endRange=750;
+% for i=708:endRange
+% 	endRange-i
+% 	%for j=1:h
+% 	for j=245:700
+% 		pixel = imgs{imNr}(i,j,:);
+% 		xy=[i;j];
+% 		%xy3D = get3Dfrom2D(xy, imNr, PcamAbs, Kcanon10GOOD, Walls, wallNr);
+% 		projectionLine = getProjectionLine(xy, PcamAbs(:,4,:), Kcanon10GOOD, PcamAbs, imNr);
+% 
+% 		xy3D = interSectPointFromLinePlane(projectionLine(1,:), projectionLine(2,:), PlanePoint0, PlanePoint1, PlanePoint2);
+% 
+% 		xy3Drotated = inv(R) * xy3D';
+% 		xy2Drotated = homog22D(inv(R) * xy3Drotated);
+% 		x(a,b)=xy2Drotated(1);
+% 		y(a,b)=xy2Drotated(2);
+% 
+% 		a=a+1;
+% 	end
+% end
+% 
+% 
+% xMin = min(min(x))
+% xMax = max(max(x))
+% yMin = min(min(y))
+% yMax = max(max(y))
+
+
+% % makes from x ranging from 0..w,0..h
+xMin = -4.7274
+xMax = -3.7646
+yMin = -0.7215
+yMax = 1.9705
+yFactor = h/(yMax-yMin);
+xFactor = w/(xMax-xMin);
+% remove offset and add 1
+y = round((y-yMin) * yFactor)+1;
+x = round((x-xMin) * xFactor)+1;
+
+
+endRange=750;
 for i=708:endRange
 	endRange-i
 	%for j=1:h
@@ -82,13 +112,16 @@ for i=708:endRange
 
 		xy3Drotated = inv(R) * xy3D';
 		xy2Drotated = homog22D(inv(R) * xy3Drotated);
-		x=-round(xFactor*xy2Drotated(1));
-		y=round(yFactor*xy2Drotated(2));
-		if x>0 && y>0
-			imgOut( x,y,:)= pixel;
-		end
+		x=xy2Drotated(1);
+		y=xy2Drotated(2);
+		% map x,y to x,y in w,h scale
+		y = round((y-yMin) * yFactor)+1;
+		x = round((x-xMin) * xFactor)+1;
+		% save pixel value on right location
+		imgOut( x,y, :) = pixel;
 	end
 end
+
 
 
 figure(); imshow(imgs{imNr});
