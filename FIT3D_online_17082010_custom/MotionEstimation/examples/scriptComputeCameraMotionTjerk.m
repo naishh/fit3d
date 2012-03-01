0cedcfe544e0b5c70ecb148461112e2521a545b0
Naishh <webnaish@gmail.com>
2012-03-01 01:11:29 +0100

    a

--- FIT3D_online_17082010_custom/MotionEstimation/examples/scriptComputeCameraMotionTjerk.m
+++ FIT3D_online_17082010_custom/MotionEstimation/examples/scriptComputeCameraMotionTjerk.m
@@ -27,8 +27,12 @@ load([datasetDir,'mats/K.mat']);
 % We use VL_FEAT though regular sift can be employed. Make sure to delete
 % all _BW.jpg files from the data folder!!!
 fprintf('\n\n\nEXTRACTING SIFT FEATURES\n\n\n');
-%[F,Files] = getSIFTtjerk([datasetDir,'fit3dInput/'],nrFrames,1,'VL_FEAT');
-[F,Files] = getSIFT([datasetDir,'fit3dInput/'],nrFrames,1,'VL_FEAT');
+
+%BW
+[F,Files] = getSIFT_bw([datasetDir,'fit3dInput/'],nrFrames,1,'VL_FEAT');
+
+% RGB
+%[F,Files] = getSIFT([datasetDir,'fit3dInput/'],nrFrames,1,'VL_FEAT');
 
 %% COMPUTE MOTION
 % Given the SIFT features, we match them accross frames and compute the
@@ -36,6 +40,7 @@ fprintf('\n\n\nEXTRACTING SIFT FEATURES\n\n\n');
 % extended explanation.
 fprintf('\n\n\nCOMPUTING MOTION\n\n\n');
 %[Fplus, PcamPlus, PcamX, FplusExtra,KBA] = computeCameraMotion('VL_FEAT',F, 0.4, 1, 2500, K, 8, true, '8pts', 0.001, 500);
+
 [Fplus, PcamPlus, PcamX, FplusExtra,KBA] = computeCameraMotion('VL_FEAT',F, 0.4, 1, 2500, K, 8, true, '8pts', 0.001, 500);
 
 save([datasetDir,'PcamX_',datasetName,'.mat'],'PcamX');
